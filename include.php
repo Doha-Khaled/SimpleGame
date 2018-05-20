aed03fe0ff3cdb70ae4fecfe6f1dd9e8f78ddfc8
mona0204 <mona0204>
2018-05-20 11:31:18 +0200

    include file

---
+++ include.php
@@ -0,0 +1,7 @@
+<?php
+$val1 = $list_nums[$rand_nums[0]];
+$val2 = $list_nums[$rand_nums[1]];
+$val3 = $list_nums[$rand_nums[2]];
+$val4 = $list_nums[$rand_nums[3]];
+$val5 = $list_nums[$rand_nums[4]];
+?>
